29ba1f6a09fb90b5d72572150cba5b9f350c4f99
Tom Hestamp <tom@hestamp.me>
2022-12-04 00:44:23 +0200

    init

---
+++ index.js
@@ -0,0 +1,76 @@
+import TelegramBot from 'node-telegram-bot-api'
+import express from 'express'
+import cors from 'cors'
+const token = `5702124363:AAFkGexhQJs66xmiPZkKxwcnSrn_8cxq0AY`
+const webAppUrl = 'https://tg-shop-react.vercel.app'
+
+const bot = new TelegramBot(token, { polling: true })
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Кнопка зявиться нижче, заповніть форму', {
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Заповнити форму', web_app: { url: webAppUrl + '/form' } }],
+        ],
+      },
+    })
+    await bot.sendMessage(chatId, 'Зробити замовлення', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Зробити замовлення', web_app: { url: webAppUrl } }],
+        ],
+      },
+    })
+  }
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg.web_app_data.data)
+      console.log(data)
+      bot.sendMessage(
+        chatId,
+        `Дані отримані. Ваше замовлення буде надіслано за адресою ${data?.city}, ${data?.street} and ${data?.poshta}`
+      )
+    } catch (error) {}
+  }
+})
+
+app.post('/web-data', async (req, res) => {
+  const { queryId, products, totalPrice } = req.body
+
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Успішна покупка',
+      input_message_content: {
+        message_text:
+          'Замовленя на суму ' +
+          totalPrice +
+          ' гривень. Очікуйте повідомлення з трек-номером',
+      },
+    })
+    return res.status(200).json({})
+  } catch (error) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Невдала спроба покупки',
+      input_message_content: {
+        message_text: 'Замовлення не сформовано. Спробуйте ще раз',
+      },
+    })
+    return res.status(500).json({})
+  }
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => console.log('server is running on PORT ' + PORT))
